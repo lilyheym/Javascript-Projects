9ff0222423aba6ff51babaac52f51465d331a267
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-08-14 14:37:13 -0700

    made a new document and utilized element thing

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {
+    var str = "Ow! Why did you poke me?" + " Don't do that again!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction() {
+    var sentence = "Oh no!";
+    sentence += " This program is coming to life!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
--- Basic JavaScript Projects/Ptoject1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Ptoject1_expressions_alert/JS/main.js
@@ -1,6 +1,6 @@
 var A = "This is a string?"         //These are variables
 var B = "This is the beginning of the string " + " and this is the end of the string"
-var Barbie = " Margo Robbie", Ken = " Ryan Gosling", Merman = " John Cena";
+var Barbie = " Margo Robbie", Ken = " Ryan Gosling", Merman = " John Cena ";
 var C = "Boo!"
 
 x = 7   //expression
@@ -16,3 +16,15 @@ document.write(" And so I told him, \"Silly Rabbit! Trix are for kids!\"");
 
 document.write(" Some guy in Florida just won like 1.5 billion dollars. " +"Great! Now he can leave Florida!")         //a bunch of text
 document.write(Merman)
+
+const button = document.createElement('button')
+
+button.innerText = 'Can you click me?'
+
+button.id ='mainButton'
+
+button.addEventListener('click', () => {
+    alert('Oh, you clicked me!')
+})
+ document.body.appendChild(button)
+
