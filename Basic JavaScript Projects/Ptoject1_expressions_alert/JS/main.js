7899c80489938766bb4a506bdcb645f92f0c48d8
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-08-14 13:57:26 -0700

    added stuff

--- Basic JavaScript Projects/Ptoject1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Ptoject1_expressions_alert/JS/main.js
@@ -1,5 +1,18 @@
-var A = "This is a string?"
+var A = "This is a string?"         //These are variables
+var B = "This is the beginning of the string " + " and this is the end of the string"
+var Barbie = " Margo Robbie", Ken = " Ryan Gosling", Merman = " John Cena";
+var C = "Boo!"
 
-window.alert("Hello, World!");
+x = 7   //expression
+
+window.alert("Hello, World!");  //a window alert
 
 document.write(A);
+
+document.write(B)
+
+
+document.write(" And so I told him, \"Silly Rabbit! Trix are for kids!\"");
+
+document.write(" Some guy in Florida just won like 1.5 billion dollars. " +"Great! Now he can leave Florida!")         //a bunch of text
+document.write(Merman)
