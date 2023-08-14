cccaa8071e8ca56cd9580cde22b9de5abdcf3efc
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-08-14 14:38:43 -0700

    added comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,10 +1,10 @@
 function My_First_Function() {
-    var str = "Ow! Why did you poke me?" + " Don't do that again!";
+    var str = "Ow! Why did you poke me?" + " Don't do that again!";      //function that makes the button
     document.getElementById("Button_Text").innerHTML = str;
 }
 
 function myFunction() {
     var sentence = "Oh no!";
-    sentence += " This program is coming to life!";
+    sentence += " This program is coming to life!";         //function with clickable text
     document.getElementById("Concatenate").innerHTML = sentence;
 }
