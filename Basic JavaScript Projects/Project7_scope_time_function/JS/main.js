87eca679388bc6df62733af3f511a00f07d3bd8e
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-08-15 12:25:41 -0700

    comments

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,11 @@
+var X = 6
+function Add_numbers_1() {
+    var Y = 10;
+    document.write (X + Y + "<br>")
+}
+function Add_numbers_2() {
+    var Y = 10;
+    document.write(12 + Y);
+}
+Add_numbers_1();
+Add_numbers_2();
