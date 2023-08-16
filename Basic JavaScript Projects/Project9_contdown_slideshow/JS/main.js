27f175d75d8c07da418dadcfa47edf935bee2512
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-08-16 11:25:40 -0700

    slideshow

--- Basic JavaScript Projects/Project9_contdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_contdown_slideshow/JS/main.js
@@ -2,7 +2,7 @@ function countdown() {
     var seconds = document.getElementById("seconds").value;
     function tick() {
         seconds = seconds - 1;
-        timer.innerHTML = seconds;
+        timer.innerHTML = seconds;     //timer
         var time = setTimeout(tick, 1000);
         if (seconds == -1) {
             alert("Time's up!");
@@ -11,4 +11,31 @@ function countdown() {
         }
     }
     tick();
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
